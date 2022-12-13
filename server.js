6a6236643ed44946fdfc48261358147a5736c984
miavh123 <miahug123@gmail.com>
2022-12-13 12:38:03 -0500

    added the server.js file

---
+++ server.js
@@ -0,0 +1,20 @@
+const express= require('express');
+const app = express();
+const port = 300;
+const {db} = require('./db');
+const showRouter = require('./routes/show');
+const userRouter = require ('./routes/user')
+
+
+
+app.get ('/',(req,res)=>{
+    res.json()
+})
+
+app.use('/show',showRouter);
+app.use('/user',userRouter);
+
+app.listen(port,() => {
+    db.sync()
+    console.log('Your server is live')
+})
